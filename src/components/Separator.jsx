751c4596015660682bd537cf89298486fc51db00
pg147 <xyzprathamesh147@gmail.com>
2025-02-07 18:24:50 +0530

    Minor tweaks

--- src/components/Separator.jsx
+++ src/components/Separator.jsx
@@ -1,6 +1,6 @@
 export default function Separator({ className }) {
     return (
-        <div className={`h-0.5 w-full bg-gradient-to-l from-separatorGradient/0 via-primary/70 to-separatorGradient/0 ${className}`}>
+        <div className={`h-[1px] w-full bg-gradient-to-l from-separatorGradient/0 via-primary/70 to-separatorGradient/0 ${className}`}>
 
         </div>
     )
