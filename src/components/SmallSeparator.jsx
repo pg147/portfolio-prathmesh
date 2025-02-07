4aca3a3505fbf7a559c3f2342ffb52873cb27123
pg147 <xyzprathamesh147@gmail.com>
2025-02-08 00:35:46 +0530

    Separator variant created

---
+++ src/components/SmallSeparator.jsx
@@ -0,0 +1,7 @@
+export default function SmallSeparator() {
+    return (
+        <div className={`h-[1px] w-full bg-gradient-to-r from-primary/70 to-separatorGradient/0`}>
+
+        </div>
+    )
+}
