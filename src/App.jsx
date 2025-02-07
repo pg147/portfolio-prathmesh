0f8a83fecf8106282fcdf0eaf4e919fc7fa4c509
pg147 <xyzprathamesh147@gmail.com>
2025-02-07 17:02:12 +0530

    Portfolio Base Setup done

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <div className="bg-body h-screen font-medium text-xl">App</div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        regular: "Techno-Nue-Regular",
+        medium: "Techno-Nue-Medium",
+        semibold: "Techno-Nue-Semibold",
+        bold: "Techno-Nue-Bold",
+        signature: "BrotherSignature"
+      },
+      colors: {
+        body: "#181818",
+        primary: "#F64242",
+        secondary: "#902727",
+        headingGradientPrimary: "#808080",
+        headingGradientSecondary: "#F5F5F5",
+        tile: "#242424",
+        stroke: "#393939",
+        card: "#232323",
+        blip: "#313131",
+        form: "#1D1D1D"
+      }
+    },
+  },
+  plugins: [],
+}
+
