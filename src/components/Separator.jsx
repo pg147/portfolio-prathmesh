e1baaa6f8e5fbcb9f80448ebf5f85ab487bfb3cd
pg147 <xyzprathamesh147@gmail.com>
2025-02-07 17:51:35 +0530

    Separator created with index

---
+++ src/components/Separator.jsx
@@ -0,0 +1,7 @@
+export default function Separator({ className }) {
+    return (
+        <div className={`h-0.5 w-full bg-gradient-to-l from-separatorGradient/0 via-primary/70 to-separatorGradient/0 ${className}`}>
+
+        </div>
+    )
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Footer from "./Footer";
+import Separator from "./Separator";
+
+export { Footer, Separator }
