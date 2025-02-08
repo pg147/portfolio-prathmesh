44399d5928c1c39a8177ce081e2f540c1b83a913
pg147 <xyzprathamesh147@gmail.com>
2025-02-08 20:57:41 +0530

    Work Experience mobile view optimized

--- src/components/InfoCards.jsx
+++ src/components/InfoCards.jsx
@@ -2,9 +2,9 @@ import SmallSeparator from "./SmallSeparator";
 
 export default function InfoCards({ title, tenure, logo, organization, credentials }) {
     return (
-        <div className="w-full flex flex-col gap-y-4 max-w-[425px]">
+        <div className="w-fit flex flex-col gap-y-4 max-w-[425px]">
             <div className="flex flex-col font-medium">
-                <h1 className="text-xl text-textSecondary">{title}</h1>
+                <h1 className="text-lg md:text-xl text-textSecondary">{title}</h1>
                 <div className="flex items-center gap-x-3 text-subheading">
                     <p>{tenure}</p>
                     {credentials && <span>{credentials}</span>}
@@ -12,7 +12,7 @@ export default function InfoCards({ title, tenure, logo, organization, credentia
             </div>
             <SmallSeparator />
             <div className="flex items-center gap-x-3.5">
-                {logo && <img src={logo} className="size-8" alt="org_logo" />}
+                {logo && <img src={logo} className="size-6 md:size-8" alt="org_logo" />}
                 <p className="font-regular text-textSecondary">{organization}</p>
             </div>
         </div>
