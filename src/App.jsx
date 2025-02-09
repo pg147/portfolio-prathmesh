2275bfbcdb0b46593e62d84dfa92c833f995258f
pg147 <xyzprathamesh147@gmail.com>
2025-02-09 18:54:54 +0530

    Vercel Analytics enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { HeroSection } from "./components";
+import { Analytics } from '@vercel/analytics/react';
 import Homepage from "./pages/Homepage";
 
 export default function App() {
   return (
     <>
       <Homepage />
+      <Analytics />
     </>
   )
 }
